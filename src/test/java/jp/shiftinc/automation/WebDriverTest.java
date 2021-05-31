77ed334f2c4fc7bb4949cf81ba4865c9f9a672e4
tomoo.morikawa.84 <tomoo.morikawa.84@shiftinc.jp>
2021-06-01 00:58:43 +0900

    add allure step

--- src/test/java/jp/shiftinc/automation/WebDriverTest.java
+++ src/test/java/jp/shiftinc/automation/WebDriverTest.java
@@ -2,6 +2,7 @@ package jp.shiftinc.automation;
 
 import com.codeborne.selenide.*;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 
@@ -9,11 +10,13 @@ import java.time.Duration;
 import java.util.logging.Logger;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class WebDriverTest {
     private static final java.util.logging.Logger LOGGER = Logger.getLogger(WebDriverTest.class.getName());
+    private Duration wait = Duration.ofSeconds(20);
 
     // 90.0.818.66
     // 91.0.864.37
@@ -47,9 +50,9 @@ public class WebDriverTest {
 
     private void test (){
         open("/");
-        $("a[href*=\".150.1.zip\"]").should(Condition.visible, Duration.ofSeconds(20));
-        LOGGER.info(Selenide.getUserAgent());
+        $("a[href*=\".150.1.zip\"]").should(visible, wait);
+        Allure.step(Selenide.getUserAgent());
         open("edge://version");
-        LOGGER.info($("#version").text());
+        Allure.step($("#version").should(visible, wait).text());
     }
 }
