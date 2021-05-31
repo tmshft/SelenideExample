44edcb4ee11cb38aac68f3c0098d0955cc2aa4fd
tomoo.morikawa.84 <tomoo.morikawa.84@shiftinc.jp>
2021-06-01 01:22:19 +0900

    repeat time

--- src/test/java/jp/shiftinc/automation/WebDriverTest.java
+++ src/test/java/jp/shiftinc/automation/WebDriverTest.java
@@ -36,13 +36,13 @@ public class WebDriverTest {
         SelenideLogger.removeListener("AllureSelenide");
     }
 
-    @RepeatedTest(value = 2)
+    @RepeatedTest(value = 5)
     public void edgeTest90() {
         Configuration.browserVersion = "90.0.818.66";
         test();
     }
 
-    @RepeatedTest(value = 2)
+    @RepeatedTest(value = 5)
     public void edgeTest91() {
         Configuration.browserVersion = "91.0.864.37";
         test();
